9065966be96e015abbc2abef07b0861a700a3ddb
AJinaEugen <alexandru.jina@gmail.com>
2021-08-28 13:26:16 +0300

    Finnished Allerts and started working with baseclasses and Junit annotations

---
+++ src/test/java/Base/BaseTest.java
@@ -0,0 +1,29 @@
+package Base;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+
+   public  WebDriver driver;
+
+   @Before
+    public void setup(){
+
+        System.setProperty("webdriver.chrome.driver","C:\\Automation\\Chrome driver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://demo.automationtesting.in/Register.html");   // are rolul sa mearga la url si sa astepte sa se incarce pagina
+        driver.manage().window().maximize();
+
+    }
+
+
+    @After
+    public void tearDown(){
+       driver.quit();
+    }
+
+
+}
---
+++ src/test/java/Tests/AlerteTests.java
@@ -0,0 +1,68 @@
+package Tests;
+
+import Base.BaseTest;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
+
+public class AlerteTests extends BaseTest {
+
+
+
+
+
+
+
+    @Test
+    public void testalerte(){
+
+
+        WebElement SwitcToelement = driver.findElement(By.xpath("//a[contains(text(),'Switch')]"));
+        // obiect de tipul actions  !!!!
+        Actions hover = new Actions(driver);
+        hover.moveToElement(SwitcToelement).build().perform();  // garanteaza ca merge cu mouse-ul la element
+
+        WebElement AlerElement = driver.findElement(By.cssSelector("a[href='Alerts.html']"));
+        AlerElement.click();
+
+        driver.navigate().to("http://demo.automationtesting.in/Alerts.html");  // merge si nu asteapta pagina sa se incarce.
+
+
+        List<WebElement> elementalerts = driver.findElements(By.cssSelector(".nav.nav-tabs.nav-stacked li"));
+
+        // Click pe primul element.
+        elementalerts.get(0).click();
+        WebElement AllerwithOKelement = driver.findElement(By.cssSelector("button[onclick='alertbox()']"));
+        AllerwithOKelement.click();
+
+        System.out.println(driver.switchTo().alert().getText());  // Verificam daca intelij a interactionat cu alerta
+        driver.switchTo().alert().accept();  // INteractionezi cu alerta.
+
+        elementalerts.get(1).click();
+        WebElement AllerwithOKcancelelement = driver.findElement(By.cssSelector("button[onclick='confirmbox()']"));
+        AllerwithOKcancelelement.click();
+        System.out.println(driver.switchTo().alert().getText());  // Verificam daca intelij a interactionat cu alerta
+
+        driver.switchTo().alert().dismiss();
+
+
+        elementalerts.get(2).click();
+        WebElement Allerwithtextelement = driver.findElement(By.cssSelector("button[onclick='promptbox()']"));
+        Allerwithtextelement.click();
+        driver.switchTo().alert().sendKeys("Alexandru");
+        driver.switchTo().alert().accept();
+
+
+
+
+
+
+    }
+
+
+}
